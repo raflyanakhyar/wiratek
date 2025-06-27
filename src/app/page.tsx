ac459b7646bbd0d58ca71b050e73455585ac5576
raflyanakhyar <raflyanakyar@gmail.com>
2025-06-28 02:03:28 +0700

    style: trigger table book and member

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 import { BooksTable } from "@/components/books-table";
 import { MembersTable } from "@/components/members-table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
     <>
-      <BooksTable />
-      <MembersTable />
+      <div className="container mx-auto my-10">
+        <Tabs defaultValue="books" className="w-full">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="books">Buku</TabsTrigger>
+            <TabsTrigger value="member">Member</TabsTrigger>
+          </TabsList>
+          <TabsContent value="books">
+            <BooksTable />
+          </TabsContent>
+          <TabsContent value="member">
+            <MembersTable />
+          </TabsContent>
+        </Tabs>
+      </div>
     </>
   );
 }
